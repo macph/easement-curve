077e3031e7a94ebd58331e5ac1c782acbae7d36d
macph <macph@users.noreply.github.com>
2016-11-15 22:39:03 +0000

    Fixed variable

--- build.bat
+++ build.bat
@@ -1,8 +1,11 @@
 @echo off
 
 REM Batch script to automate building executables with 32-bit Conda Python.
+REM Environment name is 'ec32'.
 REM     -f: Build one file executable.
 REM     -d: Build executable with directory.
+
+set conda_env=ec32
 
 IF "%~1" == "" (
     @echo No arguments inputted.
@@ -17,13 +20,13 @@ REM All other arguments
 
 :bf
     set CONDA_FORCE_32BIT=1
-    call activate ec32
+    call activate %conda_env%
     pyinstaller build_f.spec
     goto finish
 
 :bd
     set CONDA_FORCE_32BIT=1
-    call activate ec32
+    call activate %conda_env%
     pyinstaller build_d.spec
     goto finish
 
