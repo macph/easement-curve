73ecc5912a0f98a508f1740c07876b855a3c26b4
macph <macph@users.noreply.github.com>
2016-11-22 17:35:04 +0000

    Fixed build batch script

--- build.bat
+++ build.bat
@@ -8,22 +8,32 @@ set BUILD=build_f.spec
 set ENCODING=850
 set VERPATCH=C:\Utilities\verpatch\verpatch
 
+REM Setting arguments so they can be called by subroutines
+set ARG1="%~2"
+set ARG2="%~3"
+
 IF "%~1" == "" (
-    goto build
-    exit /b
+    call :build
+    goto eof
+)
+
+IF "%~1" == "--amend" (
+    call :add
+    goto eof
 )
 
 IF "%~1" == "--add" (
-    IF "%~2" == "" goto err
-    IF "%~3" == "" goto err
-    goto build
-    goto add
-    exit /b
+    call :build
+    call :add
+    goto eof
 )
+
+@echo Wrong arguments inputted.
+goto eof
 
 :err
     @echo Need 2nd and 3rd arguments for file and product versions respectively.
-    exit /b
+    goto eof
 
 :build
     set CONDA_FORCE_32BIT=1
@@ -33,24 +43,32 @@ IF "%~1" == "--add" (
 
     call deactivate
     set CONDA_FORCE_32BIT=
+    exit /b
 
 :add
+    IF %ARG1% == "" goto err
+    IF %ARG2% == "" goto err
+
     @echo Setting encoding to UTF-8...
     REM If using this script - be aware your PC's cmd may use a different encoding.
     REM Run 'chcp' to find out, and set ENCODING.
     chcp 65001
 
     set FILEDESCR=/s desc "Tool for calculating easement curves in TS"
-    set FILEVER="%~2"
+    set FILEVER=%ARG1%
     set INTERNAL=/s title "ec"
     set COMPANY=/s company "macph"
     set COPYRIGHT=/s copyright "© 2016 Ewan Macpherson"
     set ORIGINAL=/s OriginalFilename "ECCalc.exe"
     set PRODUCT=/s product "Easement curve calculator"
-    set PRODVER=/pv "%~3"
+    set PRODVER=/pv %ARG2%
 
     @echo Adding version file...
     %VERPATCH% /va dist\ECCalc.exe %FILEVER% %FILEDESCR% %INTERNAL% %COMPANY% %COPYRIGHT% %ORIGINAL% %PRODUCT% %PRODVER%
 
     @echo Resetting encoding...
     chcp %ENCODING%
+
+    exit /b
+
+:eof
